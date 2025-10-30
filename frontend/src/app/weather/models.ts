506b9b423976d08ded00df39511ede6b9db2ff6f
Robin <lassak.robin@gmail.com>
2025-10-30 08:52:44 +0100

    weather model

---
+++ frontend/src/app/weather/models.ts
@@ -0,0 +1,9 @@
+export interface WeatherModel{
+  location: string;
+  timestamp: string;
+  temp_celsius: number;
+  weather_description: string;
+  wind_mps: number;
+  wind_dir: string;
+  humidity: number;
+}
